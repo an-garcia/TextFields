5e4ab66917115e40bf3f0fd00a06a7e12eb0a66d
Angel Newton <xanewton@gmail.com>
2017-11-04 22:16:25 -0400

    Import initial project

--- TextFields/ViewController.swift
+++ TextFields/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Outlets
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+    // MARK: Text Field Delegate objects
+    let emojiDelegate = EmojiTextFieldDelegate()
+    let colorizerDelegate = ColorizerTextFieldDelegate()
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // set the label to be hidden
+        self.characterCountLabel.isHidden = true
+        
+        // Set the three delegates
+        self.textField1.delegate = emojiDelegate
+        self.textField2.delegate = colorizerDelegate
+        self.textField3.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Text Field Delegate Methods
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // Figure out what the new text will be, if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        // hide the label if the newText will be an empty string
+        self.characterCountLabel.isHidden = (newText.length == 0)
+        
+        // Write the length of newText into the label
+        self.characterCountLabel.text = String(newText.length)
+        
+        // returning true gives the text field permission to change its text
+        return true;
     }
 
 
